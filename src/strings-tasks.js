42dfeeb5f434cafa6f45ff3d85c605fa23f017f7
vadzimavich <vadzimavich@gmail.com>
2024-12-17 02:24:44 +0300

    feat: add formatTime function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -229,8 +229,14 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const totalSeconds = minutes * 60 + seconds;
+  const normalizedMinutes = Math.floor(totalSeconds / 60);
+  const normalizedSeconds = totalSeconds % 60;
+  const formattedMinutes = normalizedMinutes.toString().padStart(2, '0');
+  const formattedSeconds = normalizedSeconds.toString().padStart(2, '0');
+
+  return `${formattedMinutes}:${formattedSeconds}`;
 }
 
 /**
